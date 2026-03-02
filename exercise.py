4ad3672451b41dcf5e70615961e5315a906ddeed
rena <662901@qq.com>
2026-03-02 21:31:54 +0800

    学习数值、赋值、bool、字符串取值，简单代码运行

---
+++ exercise.py
@@ -0,0 +1,11 @@
+my_str='应收账款的期末余额为2000元'
+print(my_str[:4]+my_str[-6:])
+
+A='哈'
+B='我'
+front=my_str[:5]
+aMany=A*4
+middle=my_str[5:9]
+bMany=B*4
+end=my_str[-6:]
+print(front+aMany+middle+bMany+end)
--- init.py
+++ init.py
@@ -1,1 +1,39 @@
-print("今天星期五")
+# age='18'
+# age1='16'
+# name1='rena'
+
+# a=18
+# b=16
+# d=0
+# e=-1
+
+
+# age='19'
+# a=19
+# print(age)
+# print(a)
+
+# age=15
+# a=5
+# print(age)
+# print(a)
+
+# print(a>b)
+# print(b>a)
+
+my_str='应收账款的期末余额为2000元'
+# print(my_str[:4])
+# print(my_str[::-1])
+# print(my_str)
+
+print(my_str[5:9:])
+print(my_str[5:9:1])
+print(my_str[5:9:2])
+print(my_str[5:9:3])
+print(my_str[5:9:4])
+
+# print(my_str[6:10])
+# print(my_str[-5:-2])
+
+
+应收账款的哈哈哈哈期末余额我我我我为2000元
